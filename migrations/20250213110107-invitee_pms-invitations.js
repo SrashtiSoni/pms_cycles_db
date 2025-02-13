edcd67b0fbccd90d547cde8fab9d614c172656b2
Monu Carpenter <monu.carpenter@coralmango.com>
2025-02-13 17:08:39 +0530

    fix(invitations): Added deleted_at column in invitations table

--- migrations/20250213110107-invitee_pms-invitations.js
+++ migrations/20250213110107-invitee_pms-invitations.js
@@ -13,10 +13,26 @@ module.exports = {
       onUpdate: "CASCADE",
       onDelete: "SET NULL",
     });
+    await queryInterface.addColumn("pms_invitations", "deleted_at", {
+      type: Sequelize.DATE,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("pms_invitations", "deleted_by", {
+      type: Sequelize.UUID,
+      allowNull: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn("pms_invitations", "invitee");
+    await queryInterface.removeColumn("pms_invitations", "deleted_at");
+    await queryInterface.removeColumn("pms_invitations", "deleted_by");
     await queryInterface.renameColumn("pms_invitations", "inviter", "from");
   },
 };
