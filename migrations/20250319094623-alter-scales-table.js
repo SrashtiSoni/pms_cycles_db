dce09fee24a8bccb324f352ab2a303802d9098ca
prabhat <prabhat.jha@coralmango.com>
2025-03-19 17:36:59 +0530

    edited alter scale migrations

--- migrations/20250319094623-alter-scales-table.js
+++ migrations/20250319094623-alter-scales-table.js
@@ -12,26 +12,9 @@ module.exports = {
       allowNull: false,
       defaultValue: true,
     });
-    await queryInterface.removeColumn("scales", "use_smile_rating");
-    await queryInterface.removeColumn("scales", "use_rating_numbers");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn("scales", "company_id", {
-      type: Sequelize.UUID,
-      allowNull: false,
-    });
-
     await queryInterface.removeColumn("scales", "is_custom");
-    await queryInterface.addColumn("scales", "use_smile_rating", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
-    await queryInterface.addColumn("scales", "use_rating_numbers", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
   },
 };
