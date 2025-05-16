0f7a9833e6f58a708fd410b77c6a4cce380baa46
Srashti Soni <srashti.soni@coralmango.com>
2025-05-16 11:44:41 +0530

    fix

--- migrations/20250325062628-add-unique-constraint-reference-name.js
+++ migrations/20250325062628-add-unique-constraint-reference-name.js
@@ -1,13 +1,22 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addConstraint('pms_custom_review_references', {
-      fields: ['reference_name'],
-      type: 'unique',
-      name: 'unique_reference_name_constraint' 
+    await queryInterface.addConstraint("pms_custom_review_references", {
+      fields: ["reference_name"],
+      type: "unique",
+      name: "unique_reference_name_constraint",
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint('pms_custom_review_references', 'unique_reference_name_constraint');
-  }
+    try {
+      await queryInterface.removeConstraint(
+        "pms_custom_review_references",
+        "unique_reference_name_constraint"
+      );
+    } catch (err) {
+      console.warn(
+        "Constraint unique_reference_name_constraint not found, skipping remove"
+      );
+    }
+  },
 };
--- migrations/20250507060232-remove-unique-constraint-reference-name.js
+++ migrations/20250507060232-remove-unique-constraint-reference-name.js
@@ -15,15 +15,24 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint(
-      "pms_custom_review_references",
-      "unique_review_type_reference_name_constraint"
-    );
+    // Check duplicates before adding constraint
+    const [results] = await queryInterface.sequelize.query(`
+    SELECT reference_name, COUNT(*)
+    FROM pms_custom_review_references
+    GROUP BY reference_name
+    HAVING COUNT(*) > 1;
+  `);
 
-    await queryInterface.addConstraint("pms_custom_review_references", {
-      fields: ["reference_name"],
-      type: "unique",
-      name: "unique_reference_name_constraint",
-    });
+    if (results.length === 0) {
+      await queryInterface.addConstraint("pms_custom_review_references", {
+        fields: ["reference_name"],
+        type: "unique",
+        name: "unique_reference_name_constraint",
+      });
+    } else {
+      console.warn(
+        "Duplicates exist, skipping adding unique constraint on reference_name"
+      );
+    }
   },
 };
